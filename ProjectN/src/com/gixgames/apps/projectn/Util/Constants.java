a16678de6d4ca26a101c07731f25ef5223b72e3b
Terry Kwon <terry@gixgames.com>
2015-01-13 10:18:01 +0900

    dependencies / constants

---
+++ ProjectN/src/com/gixgames/apps/projectn/Util/Constants.java
@@ -0,0 +1,10 @@
+package com.gixgames.apps.projectn.Util;
+
+public class Constants {
+	  public static final String WEB_CLIENT_ID = "94332242625-pefst92t1vtc5ikq3pt9no1u9r9mchqu.apps.googleusercontent.com";
+	  public static final String ANDROID_CLIENT_ID = "94332242625-si611cu4p46gufbvgca1i72h2503au3r.apps.googleusercontent.com";
+	  public static final String IOS_CLIENT_ID = "94332242625-sh11taeh99ijt090ondcooket7mgiqqc.apps.googleusercontent.com";
+	  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+	  public static final String IOS_AUDIENCE = WEB_CLIENT_ID;
+	  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+	  }
